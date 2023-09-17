06cc957396d12c81f1900d7cc92aa3d29c1952c3
Makhan AutomationHub <makhan.dhar85@gmail.com>
2023-09-17 17:15:34 +0530

    Initial framework setup completed

--- src/test/java/com/domain/runners/TestRunner.java
+++ src/test/java/com/domain/runners/TestRunner.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-
         features = "src/test/resources/features",   // Path to your feature files
         glue = "com.domain.stepdefinitions",                 // Package containing your step definitions
         plugin = {
@@ -15,7 +14,7 @@ import org.junit.runner.RunWith;
                 "rerun:target/failed_script.txt"
         },
         monochrome = true,                          // Display console output in a readable format
-//        tags = "@smoke",                             // Run scenarios tagged as "@smoke" (customize as needed)
+        tags = "@smoke",                             // Run scenarios tagged as "@smoke" (customize as needed)
         dryRun = false
 )
 public class TestRunner {
